9bbb7f66a0f91eb4512948df1073d7ce938e7351
Andy Cameron <andy.d.cameron@gmail.com>
2017-11-28 19:33:52 +0000

    commenting and renaming

--- src/mapper.py
+++ src/mapper.py
@@ -1,87 +1,73 @@
 #!/usr/bin/env python3
 
 import sys
-import re
-from datetime import datetime
 import pandas as pd
+import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.decomposition import PCA
-from sys import argv
+import random
 
 
-def get_file_array():
+def file_names():
     return sys.argv[1:]
 
 
-def get_docs_file_array(file_array):
-    docs, fnames = [], []
-    for fname in file_array:
-        try:
-            with open(fname, 'rt') as f:
-                docs.append(' '.join(f.readlines()))
-            fnames.append(fname)
-        except Exception as e:
-            with open('error.log', 'at') as f:
-                f.write("{} :: Skipping file {} :: {}\n".format(
-                    datetime.now(), fname, e))
-    return docs, fnames
+def get_words(fname):
+    with open(fname, 'rt') as f:
+        return ' '.join([l for l in f.readlines()])
 
 
-def get_df(docs, file_array):
-    # Will also need to get rid of words with a leading underscore - they are useless
-    # Maybe get word stems as well? - have link for that
+def remove_unwanted_words(words):
+    # Do it here
+    return ' '.join([word for word in words.split(' ') if True])
 
+
+def vectorise(words_lists, file_names):
     # https://stackoverflow.com/questions/15899861/efficient-term-document-matrix-with-nltk
     vec = CountVectorizer()
-    X = vec.fit_transform(docs)
-    df = pd.DataFrame(X.toarray(), columns=vec.get_feature_names(), index=[
-                      i for i in file_array])  # set up the table
-    df.to_csv("vector_table.csv", encoding='utf-8', index=True)
+    X = vec.fit_transform(words_lists)
+    df = pd.DataFrame(X.toarray(),
+                      columns=vec.get_feature_names(),
+                      index=[i for i in file_names])  # set up the table
     return df
 
 
-def print_output(df):
-    # https://www.shanelynn.ie/select-pandas-dataframe-rows-and-columns-using-iloc-loc-and-ix/
-    # that's taking the values from all the rows, excluding the first column
-    int_values = df.values[:, 1:].tolist()
-    filename_values = df.index.values.tolist()
-
-    # Iterate over the list, make the output
-    count = 0
-    for i in int_values:
-        # This will be
-        # what is outputted to the second mapper/reducer
-        s = 'key\t{}'.format(filename_values[count])
-        s += ' ' + ' '.join([str(n) for n in i])
-        print(s)
-        # print('key\t{},{}'.format(v for v in filename_values[count], i))
-        count += 1
+def distance(vector, vector2):
+    return np.sqrt(np.sum(np.square((vector - vector2))))
 
 
-def reduce_dimensions(df, n_components=None):
-    """
-    @args: data - a numpy array containing arrays of numbers e.g.: [[1,2,3], [4,5,6], ...]
-    @returns: a new numpy array containing arrays of numbers
-    """
-    # Use Principle Component Analysis to reduce the dimensions
-    # http://scikit-learn.org/stable/modules/generated/sklearn.decomposition.PCA.html
-    # TODO: Can specify n_components in when initialising PCA. Not sure if/what should be used.
-    # return PCA().fit_transform(df)
-    return pd.DataFrame(
-        PCA(n_components).fit_transform(df),
-        index=df.index
-    )
+def nearest_centroid(centroids, vector):
+    nearest, dist = None, float('inf')
+    for i, c in enumerate(centroids):
+        if distance(c, vector) < dist:
+            nearest = centroids[i]
+    return nearest
 
 
 def main():
-    file_array = get_file_array()
-    # a new file_array is returned because if any of the files cause errors
-    # we need to know which files were actually used
-    docs, file_array = get_docs_file_array(file_array)
-    df = get_df(docs, file_array)
-    # n_components = 50  # n components for reducing dimensions
-    df = reduce_dimensions(df)
-    print_output(df)
+    N_CENTROIDS = 5
+    # get the file names
+    fnames = file_names()
+
+    # read the words from the files into a list of strings
+    file_words = []
+    for fname in fnames:
+        try:
+            words = get_words(fname)
+            file_words.append(words)
+        except:
+            fnames.remove(fname)
+
+    # vectorise the files words
+    vectors = vectorise(file_words, fnames)
+
+    # pick random centroids
+    random_centroids = set()
+    while len(random_centroids) != N_CENTROIDS:
+        random_centroids.add(vectors[random.randint(0, len(vectors))])
+
+    # print the nearest centroid as a key, the vector as the value
+    for v in vectors:
+        print("{}\t{}".format(nearest_centroid(random_centroids, v), v))
 
 
 main()
---
+++ src/reducer.py
@@ -0,0 +1,29 @@
+# input: centroid, [a list of points]
+# output: a new centroid
+
+import sys
+import numpy as np
+
+
+def get_centroid_and_vectors():
+    """read the input from command line"""
+    centroid = sys.argv.split('\t')[0]
+
+    lines = sys.argv.split('\t')[1]
+    lines = [l.rstrip('\n') for l in lines]
+    vectors = [
+        float(v) for l in lines
+        for v in l.split(' ')
+    ]
+    return centroid, np.array(vectors)
+
+
+def calc_new_centroid(vectors):
+    """calculate the new centroid from all the vectors"""
+    return np.mean(vectors, axis=0)
+
+
+def main():
+    original_centroid, vectors = get_centroid_and_vectors()
+    new_centroid = calc_new_centroid(vectors)
+    print("{}\t{}".format(original_centroid, new_centroid))
