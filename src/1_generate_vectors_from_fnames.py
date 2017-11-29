ec684f1fc6a7a8c3ddb0d7cc6a1aeb1b2c3d4120
Emmabyrne36 <emmabyrne36@gmail.com>
2017-11-29 12:59:13 +0000

    added reducing doc space

--- src/1_generate_vectors_from_fnames.py
+++ src/1_generate_vectors_from_fnames.py
@@ -20,6 +20,30 @@ def words(fname):
     with open(fname, 'rt') as f:
         return ' '.join([l for l in f.readlines()])
 
+def find_cols_to_delete(stopwords, df):
+    headers = list(df)
+    # add stopwords to the cols to drop
+    columns_to_drop = [] + stopwords
+    index = 0
+    for column in df:
+        total_num = df[column].sum(axis=None) # sum the values in the columns
+        if total_num <= 1: # if the total is <= 1 - if the word only appears in one document, add to drop list
+            columns_to_drop.append(headers[index])
+        index += 1
+    return columns_to_drop
+
+def drop_columns(df, columns_to_drop):
+    # This is original df
+    # output as csv
+    # iterate over it, drop the cols
+    # return the updated dataframe
+    stopwords = ['i', 'in', 'the', 'if', 'or', 'it']
+    columns_to_drop = find_cols_to_delete(stopwords, df)
+    for i in columns_to_drop:
+        df.drop(i, axis=1) # drop the columns
+
+    return df
+
 
 def vectorise(fname_words_dict):
     # https://stackoverflow.com/questions/15899861/efficient-term-document-matrix-with-nltk
@@ -28,6 +52,10 @@ def vectorise(fname_words_dict):
     df = pd.DataFrame(X.toarray(),
                       columns=vec.get_feature_names(),
                       index=[i for i in fname_words_dict.keys()])  # set up the table
+    df.to_csv("vector_table_before.csv", encoding='utf-8', index=True)
+    # remove stopwords etc.
+    df = drop_columns(df, find_cols_to_delete())
+    df.to_csv("vector_table_after.csv", encoding='utf-8', index=True)
     return zip(fname_words_dict.keys(), df.as_matrix())
 
 
