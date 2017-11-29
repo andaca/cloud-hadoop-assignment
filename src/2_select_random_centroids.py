19407c2b09a85321a8d94f3f7dea65afa5f21fd0
Andy Cameron <andy.d.cameron@gmail.com>
2017-11-29 15:25:10 +0000

    parts 1 and 2 working

--- src/2_select_random_centroids.py
+++ src/2_select_random_centroids.py
@@ -1,9 +1,55 @@
+#!/usr/bin/env python3
 """
 
-IN:     "key"       <vector>,<vector>,<vector>,...
+IN:     fname       <vector>
+        fname       <vector>
+        fname       <vector>
+        ...
 
 OUT:    <vector>    <centroid>,<centroid>,<centroid>,...
         <vector>    <centroid>,<centroid>,<centroid>,...
         <vector>    <centroid>,<centroid>,<centroid>,...
         ...
 """
+import random
+import sys
+
+
+def get_vectors():
+    vector_strings = [l.split('\t')[1] for l in sys.stdin]
+    vector_strings = [vs.rstrip('\n') for vs in vector_strings]
+    vectors = [vs.split(' ') for vs in vector_strings]
+
+    for i, v in enumerate(vectors):
+        v = [i for i in v if i]
+        vectors[i] = [int(i) for i in v]
+    return vectors
+
+
+def stringify_vector(vector):
+    return ' '.join([str(i) for i in vector])
+
+
+def main():
+    N_CENTROIDS = 5
+
+    vectors = get_vectors()
+
+    # pick random centroids
+    # Use a set to ensure they are all unique.
+    # Sets have to be of tuples rather than lists, since lists are mutable
+    centroids = set()
+    while len(centroids) != N_CENTROIDS:
+        centroids.add(tuple(vectors[random.randint(0, len(vectors) - 1)]))
+
+    # Now you can cast the set of tuples to a list of lists
+    centroids = [list(c) for c in centroids]
+
+    for v in vectors:
+        print("{}\t{}".format(
+            stringify_vector(v),
+            ','.join([stringify_vector(c) for c in centroids])
+        ))
+
+
+main()
---
+++ src/README.md
@@ -0,0 +1,5 @@
+parts 1 and 2 seem to be working. to run:
+
+```
+$ python3 1_generate_vectors_from_fnames.py sport/5*txt | python3 2_select_random_centroids.py
+```
