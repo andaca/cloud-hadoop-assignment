b13315c8cf036722bc208cbcb64a4d1b5c915e7a
Andy Cameron <andy.d.cameron@gmail.com>
2017-11-29 15:53:34 +0000

    1, 2, and 3 seem to all work together

--- src/3_nearest_centroid_to_vector.py
+++ src/3_nearest_centroid_to_vector.py
@@ -13,35 +13,20 @@ from sklearn.feature_extraction.text import CountVectorizer
 import random
 
 
-def file_names():
-    return sys.argv[1:]
-
-
-def get_words(fname):
-    with open(fname, 'rt') as f:
-        return ' '.join([l for l in f.readlines()])
-
-
-def remove_unwanted_words(words):
-    # Do it here
-    return ' '.join([word for word in words.split(' ') if True])
-
-
-def vectorise(words_lists, file_names):
-    # https://stackoverflow.com/questions/15899861/efficient-term-document-matrix-with-nltk
-    vec = CountVectorizer()
-    X = vec.fit_transform(words_lists)
-    df = pd.DataFrame(X.toarray(),
-                      columns=vec.get_feature_names(),
-                      index=[i for i in file_names])  # set up the table
-    return df.as_matrix()
+def get_vector_centroids():
+    vector, centroids = list(sys.stdin)[0].split('\t')
+    vector = [int(n) for n in vector.split(' ')]
+    centroids = centroids.split(',')
+    for i, c in enumerate(centroids):
+        centroids[i] = [int(n) for n in c.split(' ')]
+    return vector, centroids
 
 
 def distance(vector, vector2):
     return np.sqrt(np.sum(np.square((vector - vector2))))
 
 
-def nearest_centroid(centroids, vector):
+def nearest_centroid(vector, centroids):
     nearest, dist = None, float('inf')
     for i, c in enumerate(centroids):
         if distance(c, vector) < dist:
@@ -49,33 +34,20 @@ def nearest_centroid(centroids, vector):
     return nearest
 
 
+def stringify_vector(vector):
+    return ' '.join([str(i) for i in vector])
+
+
 def main():
-    N_CENTROIDS = 5
-    # get the file names
-    fnames = file_names()
+    vector, centroids = get_vector_centroids()
+    vector = np.array(vector)
+    centroids = np.array(centroids)
+    nearest_cent = nearest_centroid(vector, centroids)
 
-    # read the words from the files into a list of strings
-    file_words = []
-    for fname in fnames:
-        try:
-            words = get_words(fname)
-            file_words.append(words)
-        except:
-            fnames.remove(fname)
-
-    # vectorise the files words
-    vectors = vectorise(file_words, fnames)
-    print(vectors)
-    # pick random centroids
-    random_centroids = set()
-    while len(random_centroids) != N_CENTROIDS:
-        random_centroids.add(
-            vectors[random.randint(0, len(vectors))]
-        )
-
-    # print the nearest centroid as a key, the vector as the value
-    for v in vectors:
-        print("{}\t{}".format(nearest_centroid(random_centroids, v), v))
+    print("{}\t{}".format(
+        stringify_vector(nearest_cent),
+        stringify_vector(vector)
+    ))
 
 
 main()
