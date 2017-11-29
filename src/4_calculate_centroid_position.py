94e3daf1af97b1bdc3dd24c700250eb90dd5d6a0
Andy Cameron <andy.d.cameron@gmail.com>
2017-11-29 18:34:05 +0000

    4 seems to work, but it's only receiing one vector

--- src/4_calculate_centroid_position.py
+++ src/4_calculate_centroid_position.py
@@ -1,6 +1,10 @@
 
 """
-IN:     <prev_centroid>     <vector>,<vector>,<vector>,...
+IN:     <prev_centroid>     <vector>
+        <prev_centroid>     <vector>
+        <prev_centroid>     <vector>
+        ...
+
 OUT:    <prev_centroid>     <new_centroid>
 """
 
@@ -9,24 +13,35 @@ import numpy as np
 
 
 def get_centroid_and_vectors():
-    """read the input from command line"""
-    centroid = sys.argv.split('\t')[0]
+    """read the input from stdin"""
 
-    lines = sys.argv.split('\t')[1]
-    lines = [l.rstrip('\n') for l in lines]
-    vectors = [
-        float(v) for l in lines
-        for v in l.split(' ')
-    ]
-    return centroid, np.array(vectors)
+    vectors = []
+    for l in sys.stdin:
+        print('sdf')
+        centroid, vector = l.split('\t')
+        vector = [int(i) for i in vector.split(' ')]
+        vectors.append(vector)
+
+    return centroid, vectors
 
 
 def calc_new_centroid(vectors):
     """calculate the new centroid from all the vectors"""
-    return np.mean(vectors, axis=0)
+    return list(np.mean(vectors, axis=0))
+
+
+def stringify_vector(vector):
+    return ' '.join([str(i) for i in vector])
 
 
 def main():
     original_centroid, vectors = get_centroid_and_vectors()
+
     new_centroid = calc_new_centroid(vectors)
-    print("{}\t{}".format(original_centroid, new_centroid))
+    print("{}\t{}".format(
+        original_centroid,
+        stringify_vector(new_centroid)
+    ))
+
+
+main()
